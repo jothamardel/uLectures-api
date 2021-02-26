78d1a7818bfb6c7dfd77130388ef44d218104b69
jothamardel <ardelmbiplang@gmail.com>
2021-02-26 08:33:11 +0100

    Creating models

---
+++ model/content.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const contentSchema = Schema({
+  course_id: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  video: [
+    {
+      video_title: {
+        type: String,
+        required: true
+      },
+      url: {
+        type: String,
+        required: true,
+        unique: true
+      }
+    }
+  ]
+});
+
+
+module.exports = mongoose.model('Content', contentSchema);
